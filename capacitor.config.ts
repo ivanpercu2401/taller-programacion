01252f2d56c8086da1d53a7c6b48d95043ad1031
ivanpercu2401 <idperdomo-2022a@corhuila.edu.co>
2024-04-29 23:59:54 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'taller_programacion',
+  webDir: 'dist'
+};
+
+export default config;
